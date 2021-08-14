430f3dd906cfc55452cf9b383c1bdef3c44481de
Paulo Ferreira <paulo.ferreira@ditech.com.br>
2021-08-14 17:16:47 -0300

    Implementing WebService Service

---
+++ GoodWeather/Services/Services.swift
@@ -0,0 +1,31 @@
+//
+//  Services.swift
+//  GoodWeather
+//
+//  Created by Junior Ferreira on 14/08/21.
+//
+
+import Foundation
+
+
+struct Resource<T> {
+    let url: URL
+    let parse: (Data) -> T?
+}
+
+final class Weservice {
+    
+    func load<T>(resource: Resource<T>, completion: @escaping (T?) -> ()) {
+        
+        URLSession.shared.dataTask(with: resource.url) { data, response, error in
+            
+            if let data = data {
+                DispatchQueue.main.async {
+                    completion(resource.parse(data))
+                }
+            } else {
+                completion(nil)
+            }
+        }.resume()
+    }
+}
