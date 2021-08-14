cb6d174cbcccd3bc8680c31ca6e544426d399761
Paulo Ferreira <paulo.ferreira@ditech.com.br>
2021-08-14 19:19:32 -0300

    Creating SettingsTableViewController

---
+++ GoodWeather/Controllers/SettingsTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.swift
+//  GoodWeather
+//
+//  Created by Junior Ferreira on 14/08/21.
+//
+
+import Foundation
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+    
+}
