0a4cb35ab1ed9018e77f765a9f127b97e170ac37
Paulo Junior <jr@live.at>
2021-08-15 14:51:38 -0300

    Fixing the Temperature Unit Issue

--- GoodWeather/Utils/Constants.swift
+++ GoodWeather/Utils/Constants.swift
@@ -13,10 +13,10 @@ struct Constants {
         
         static func urlForWeatherByCity (city: String) -> URL {
             
-            //let userDefaults = UserDefaults.standard
-            //let unit = (userDefaults.value(forKey: "unit") as? String) ?? "imperial"
+            let userDefaults = UserDefaults.standard
+            let unit = (userDefaults.value(forKey: "unit") as? String) ?? "imperial"
             
-            return URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city.escaped())&appid=3b2b727479adc7c07db5d00f1b08d3dd&units=imperial")!
+            return URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city.escaped())&appid=3b2b727479adc7c07db5d00f1b08d3dd&units=\(unit)")!
         }
         
     }
